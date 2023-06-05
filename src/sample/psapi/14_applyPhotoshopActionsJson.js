7587cf664db8e86e77c687b3885a74b44335b154
Nick Belofastow <nbelofastow@adobe.com>
2023-06-05 16:55:42 -0700

    Minor fixes for actionJson sample

--- src/sample/psapi/14_applyPhotoshopActionsJson.js
+++ src/sample/psapi/14_applyPhotoshopActionsJson.js
@@ -18,7 +18,7 @@ async function main() {
 
     const input = {
       href: 'https://raw.githubusercontent.com/adobe/adobe-photoshop-api-sdk/main/testfiles/input/input03.jpg',
-      storage: 'adobe'
+      storage: 'external'
     }
   
     const output = {
@@ -27,15 +27,75 @@ async function main() {
       type: sdk.psApiLib.MimeType.PSD
     }
   
+    /*
+      actionJson performs:
+      - apply traceContour filter to the original image
+      - place the additional image in a new layer (note the placeholder ACTION_JSON_OPTIONS_ADDITIONAL_IMAGES_0 for path field) and resize
+      - rename the newly created layer
+    */
     const options = {
       actionJSON: [
-        {'_obj':'traceContour','edge':{'_enum':'contourEdge','_value':'upper'},'level':148},
-        {'_obj':'placeEvent','freeTransformCenterState':{'_enum':'quadCenterState','_value':'QCSAverage'},'height':{'_unit':'percentUnit','_value':51.83142658062201},'null':{'_kind':'local','_path':'ACTION_JSON_OPTIONS_ADDITIONAL_IMAGES_0'},'offset':{'_obj':'offset','horizontal':{'_unit':'pixelsUnit','_value':298.34811239846806},'vertical':{'_unit':'pixelsUnit','_value':127.16503382715794}},'replaceLayer':{'_obj':'placeEvent','to':{'_id':5,'_ref':'layer'}},'width':{'_unit':'percentUnit','_value':51.83142658062201}}
+        {
+          '_obj': 'traceContour',
+          'edge': {
+            '_enum': 'contourEdge',
+            '_value': 'upper'
+          },
+          'level': 148
+        },
+        {
+          '_obj': 'placeEvent',
+          'freeTransformCenterState': {
+            '_enum': 'quadCenterState',
+            '_value': 'QCSAverage'
+          },
+          'height': {
+            '_unit': 'percentUnit',
+            '_value': 51.83142658062201
+          },
+          'null': {
+            '_kind': 'local',
+            '_path': 'ACTION_JSON_OPTIONS_ADDITIONAL_IMAGES_0'
+          },
+          'offset': {
+            '_obj': 'offset',
+            'horizontal': {
+              '_unit': 'pixelsUnit',
+              '_value': 298.34811239846806
+            },
+            'vertical': {
+              '_unit': 'pixelsUnit',
+              '_value': 127.16503382715794
+            }
+          },
+          'replaceLayer': {
+            '_obj': 'placeEvent',
+            'to': {
+              '_id': 5,
+              '_ref': 'layer'
+            }
+          },
+          'width': {
+            '_unit': 'percentUnit',
+            '_value': 51.83142658062201
+          }
+        },
+        {
+          '_obj': 'set',
+          '_target': [{
+            '_enum': 'ordinal',
+            '_ref': 'layer'
+          }],
+          'to': {
+            '_obj': 'layer',
+            'name': 'Overlay'
+          }
+        }
       ],
       additionalImages: [
         {
           href: 'https://raw.githubusercontent.com/adobe/adobe-photoshop-api-sdk/main/testfiles/input/input02.jpg',
-          storage: 'adobe'
+          storage: 'external'
         }
       ]
     }
@@ -43,7 +103,7 @@ async function main() {
     const job = await client.applyPhotoshopActionsJson(input, output, options)
     console.log(`${job.isDone()} - ${job.jobId}`)
     console.log(`Response: ${JSON.stringify(job,null,2)}`)
-    console.log(`Output File: ${await awsLib.getSignedUrl('getObject', 'output/test13.png')}\n`)
+    console.log(`Output File: ${await awsLib.getSignedUrl('getObject', 'output/test14.png')}\n`)
 
   } catch (e) {
     console.error(e)
